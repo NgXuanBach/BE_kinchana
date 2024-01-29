5cb817620b692887462dae284a809b840832f3e4
Nguyễn Xuân Bách <nguyenbach.working@gmail.com>
2024-01-29 21:20:49 +0700

    first commit

---
+++ kinchana/src/main/java/com/socialmedia/kinchana/KinchanaApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.kinchana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinchanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinchanaApplication.class, args);
+	}
+
+}
---
+++ kinchana/src/test/java/com/socialmedia/kinchana/KinchanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.kinchana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinchanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
