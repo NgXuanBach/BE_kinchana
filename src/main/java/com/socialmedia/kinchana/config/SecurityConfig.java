b3e273975ca0036ff55543fc53fd4a4e4d9746a4
nhatphat112 <127173620+nhatphat112@users.noreply.github.com>
2024-02-25 15:42:42 +0700

    ok

--- src/main/java/com/socialmedia/kinchana/config/SecurityConfig.java
+++ src/main/java/com/socialmedia/kinchana/config/SecurityConfig.java
@@ -1,2 +1,21 @@
-package com.socialmedia.kinchana.config;public class SecurityConfig {
+package com.socialmedia.kinchana.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.csrf().disable().build();
+        return security.build();
+    }
 }
